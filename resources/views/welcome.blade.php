e104991436e7b3f01348041ef73e7755a8d55ddc
AlmDiana <diana.almeida01@epn.edu.ec>
2022-03-07 09:31:45 -0500

    actualizado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" href="{{asset('encrypted.png') }}">
+        
         <title>Prison System Backend</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link href="{{asset('css/wel.css') }}" rel="stylesheet">
+        <link href="{{asset('static/wel.css') }}" type="text/css" rel="stylesheet">
 
     </head>
     <body class="container-fluid">
